c1ba7ed94d03b312a836db8112c3e5023dac0bfc
Paul Leclerc <paulleclerc@icloud.com>
2018-12-18 15:44:54 +0100

    Creating 4 characters with their default values.

--- monSuperRPG/main.swift
+++ monSuperRPG/main.swift
@@ -10,10 +10,12 @@ class Team {
     let teamColor: String
     let playerName: String
     
+    
     init(teamColor: String) {
         self.teamColor = teamColor
         print("Création de l'équipe \(teamColor)")
         
+        //Je demande le nom du joueur, s'il ne répond rien, il doit réécrire son nom
         print("Nom du joueur \(self.teamColor) :")
         var line = readLine()!
         while line == "" {
@@ -26,6 +28,48 @@ class Team {
     }
 }
 
+// Création des personnages
+class Character {
+    let life: Int
+    let maxLife: Int
+    
+    init(maxLife: Int) {
+//        Lors de l'initialisation, on considère que le personnage a toute sa vie.
+        self.maxLife = maxLife
+        self.life = maxLife
+    }
+}
+
+class Fighter: Character {
+    init() {
+        super.init(maxLife: 100)
+    }
+    
+}
+
+class Mage: Character {
+    //    Bonus : Le mage est capable d'utiliser la fonction Copier/Coller Une fois dans la partie, Il devient l'un des personnages de l'équipe adverse jusqu'à la fin de la partie (C'est un copier/coller complet : Il reprend toutes les propriétés du personnage adverse
+    init() {
+        super.init(maxLife: 50)
+    }
+}
+
+class Colossus: Character {
+    init() {
+        super.init(maxLife: 1500)
+    }
+}
+
+class Dwarf: Character {
+    init() {
+        super.init(maxLife: 30)
+    }
+}
+
+
+//
+//  Début de la partie, création de 2 équipes : rouge et bleue
+//
 var redTeam = Team(teamColor: "rouge")
 print("")
 var blueTeam = Team(teamColor: "bleue")
