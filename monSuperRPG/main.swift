4755992d31da84c93f9ad8a8c0680b1ea0368de8
Paul Leclerc <paulleclerc@icloud.com>
2018-12-18 14:29:56 +0100

    Initial Commit

---
+++ monSuperRPG/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  monSuperRPG
+//
+//  Created by Paul Leclerc on 18/12/2018.
+//  Copyright © 2018 Paul Leclerc. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
